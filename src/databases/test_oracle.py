15743cab65832f36f25700f1044035857793a476
KIVAR <ravi.sikhara@gmail.com>
2022-04-09 11:51:48 +1000

    Scripts to test Oracle connectivity

--- src/databases/test_oracle.py
+++ src/databases/test_oracle.py
@@ -2,35 +2,47 @@ import os
 import platform
 
 import cx_Oracle
+import sys
 
 # -------------------------------------------------------------------------------------------------#
 # Check Oracle DB Connectivity                                                                     #
 # -------------------------------------------------------------------------------------------------#
+
 # Set this to instant client location
-LOCATION = r""
+INSTANT_CLIENT_LOCATION = r"C:\Users\ravis\Desktop\ravi\instantclient_21_3"
 
-print("ARCH:", platform.architecture())
-print("FILES AT LOCATION:")
+print("Platform Architecture:", platform.architecture())
+print(f"Files available @ {INSTANT_CLIENT_LOCATION}")
 
-for name in os.listdir(LOCATION):
+for name in os.listdir(INSTANT_CLIENT_LOCATION):
     print(name)
 
-os.environ["PATH"] = LOCATION + ";" + os.environ["PATH"]
-cx_Oracle.init_oracle_client(lib_dir=LOCATION)
+#os.environ["PATH"] = LOCATION + ";" + os.environ["PATH"]
 
-user = ""
+## Upgrade cx_Oracle
+## The init_oracle_client() function was introduced in 8.0
+## python -m pip install --upgrade cx_Oracle
+cx_Oracle.init_oracle_client(lib_dir=INSTANT_CLIENT_LOCATION)
+
+user = "admin"
 password = ""
-host = ""
-port = ""
-service = ""
+host = "database-1.c7bxy1mpyupz.us-east-2.rds.amazonaws.com"
+port = "1521"
+service = "ORCL"
 
-conn = cx_Oracle.connect(f"{user}/{password}@{host}:{port}/{service}")
-cur = conn.cursor()
+try:
+    conn = cx_Oracle.connect(f"{user}/{password}@{host}:{port}/{service}")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM V$VERSION")
 
-cur.execute("SELECT * FROM V$VERSION")
+    for result in cur:
+        print(result)
 
-for result in cur:
-    print(result)
+    cur.close()
+    conn.close()
 
-cur.close()
-conn.close()
+    print("You're able to talk to the DB")
+except Exception as err:
+    print("Error connecting to the DB.")
+    print(err)
+    sys.exit(1)
--- src/databases/test_oracle_sqlalchemy.py
+++ src/databases/test_oracle_sqlalchemy.py
@@ -1,15 +1,46 @@
-import warnings
+import os
 
+# When I tried to execute this script from Windows CLI (Not Anaconda CLI)
+# getting this exception:
+#
+#    ImportError: Unable to import required dependencies:
+#    numpy: DLL load failed: The specified module could not be found.
+#
+# To fix it, I am changing the PATH to point to Anaconda library where
+# the dlls are available. 
+
+# This fix is applied for Windows.
+
+module_path = [
+    r'C:\Users\ravis\Anaconda3\Library\bin',
+]
+
+old_path = os.environ['PATH']
+new_path = ";".join(module_path)
+os.environ['PATH'] = f"{new_path};{old_path}"
+
+import warnings
 import pandas as pd
 import sqlalchemy
 from sqlalchemy import exc as sa_exc
 from sqlalchemy.exc import SQLAlchemyError
+import cx_Oracle
 
-user = ""
+INSTANT_CLIENT_LOCATION = r"C:\Users\ravis\Desktop\ravi\instantclient_21_3"
+
+# Use either of these.
+#    1. Call init_oracle_client()  or
+#    2. Set PATH  
+
+# cx_Oracle.init_oracle_client(lib_dir=INSTANT_CLIENT_LOCATION)
+
+os.environ['PATH'] = INSTANT_CLIENT_LOCATION + ";" + os.environ['PATH']
+
+user = "admin"
 password = ""
-host = ""
-port = ""
-service = ""
+host = "database-1.c7bxy1mpyupz.us-east-2.rds.amazonaws.com"
+port = "1521"
+service = "ORCL"
 
 try:
     with warnings.catch_warnings():
@@ -28,4 +59,3 @@ try:
 except SQLAlchemyError as e:
     error = str(e.__dict__["orig"])
     print(error)
-    raise e
